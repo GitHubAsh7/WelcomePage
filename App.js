99bd944799a5caf4977a4f3aa5b96608df13b5ef
GitHubAsh7 <110453700+GitHubAsh7@users.noreply.github.com>
2022-08-10 17:13:29 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//screens
+import Login from './screens/login';
+import Signup from './screens/signup';
+import Welcome from './screens/welcome';
+
+//React Navigator Stack
+import RootStack from './navigators/RootStack';
+
+export default function App() {
+  return <Welcome />;
+}
+
+
